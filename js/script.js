c9d28b72344605d686a11c94e6829befadfef8d4
Malinda Yuniarti <55964602+masha82@users.noreply.github.com>
2026-02-03 13:44:15 +0700

    fix submit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// menampilkan nama atau guest
 welcomeMessage();
 
 function welcomeMessage() {
@@ -11,5 +12,39 @@ function welcomeMessage() {
 
     //menampilkan pesan awal buka web
     document.getElementById("welcome").innerText = `Hello, ${userResponse} - Selamat datang di Linda Company`;
+}
+
+// menampilkan alert ketika isian kosong saat submit
+submitMessage();
+
+function submitMessage() {
+document.getElementById("messageForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const fields = ["name", "email", "message"];
+    let valid = true;
+
+    fields.forEach(contact => {
+        const input = document.getElementById(contact);
+
+        if (input.value.trim() === "") {
+            input.classList.add("border-red-500");
+            input.classList.remove("border-gray-300");
+            valid = false;
+        } else {
+            input.classList.remove("border-red-500");
+            input.classList.add("border-gray-300");
+        }
+    });
+
+    if (!valid) {
+        alert("⚠️ Harap isi semua field!");
+        return;
+    }
+
+    alert("Pesan berhasil dikirim 🎉");
+    this.submit();
+});
+
 }
 
